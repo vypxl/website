00e44d18b16f699435a88c86b72e2b16a0f259e9
vypxl <vypxl0+coding@gmail.com>
2020-05-07 16:34:43 +0200

    setup github repo

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     siteName: 'vypxl',
+    siteUrl: 'https://vypxl.io',
     siteDescription: 'vypxl personal website, home for my projects and blog',
 
     templates: {
@@ -8,7 +9,21 @@ module.exports = {
         Series: '/blog/series/:slug',
     },
 
-    plugins: [],
+    plugins: [
+        {
+            use: '@gridsome/plugin-sitemap',
+            options: {
+                cacheTime: 600000,
+                exclude: [],
+                config: {
+                    '/blog': {
+                        changefreq: 'weekly',
+                        priority: 0.8,
+                    },
+                },
+            },
+        },
+    ],
 
     transformers: {},
 }
