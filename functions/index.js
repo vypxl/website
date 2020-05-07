b2d5a6f300f5a6c406dc45379b3857069dd6e1c1
vypxl <vypxl0+coding@gmail.com>
2020-05-07 18:54:56 +0200

    fix triggerBuild function

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const functions = require('firebase-functions');
+const fetch = require('node-fetch')
 
 exports.triggerBuild = functions.region('europe-west3').https.onRequest(async (req, res) => {
     const pass = req.query.pass;
     if (pass == functions.config().self.pass) {
-        const response = await fetch({
-            url: 'https://api.github.com/repos/vypxl/website/dispatches',
+        const response = await fetch('https://api.github.com/repos/vypxl/website/dispatches', {
             method: 'POST',
             headers: {
                 'Accept': 'application/vnd.github.everest-preview+json',
@@ -14,8 +14,11 @@ exports.triggerBuild = functions.region('europe-west3').https.onRequest(async (r
                 event_type: 'build',
             })
         });
+        res.status(200);
+        res.end("done.")
+    } else {
+        res.status(403)
+        res.end("invalid pass");
     }
 
-    res.status(204);
-    res.end("");
 })
